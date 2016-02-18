93f22e3c0cdf93d8058d55b3b9dfc3a1ae96d878
Igor Vinokur <ivinokur@codenvy.com>
2016-02-18 17:18:23 +0200

    fixup! CHE-485: Add git revert action
    Signed-off-by: Igor Vinokur <ivinokur@codenvy.com>

--- plugins/plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/action/RevertAction.java
+++ plugins/plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/action/RevertAction.java
@@ -30,6 +30,7 @@ import org.eclipse.che.ide.ext.git.client.compare.ComparePresenter;
 import org.eclipse.che.ide.ext.git.client.compare.changedList.ChangedListPresenter;
 import org.eclipse.che.ide.part.explorer.project.ProjectExplorerPresenter;
 import org.eclipse.che.ide.project.node.FileReferenceNode;
+import org.eclipse.che.ide.project.node.FolderReferenceNode;
 import org.eclipse.che.ide.project.node.ResourceBasedNode;
 import org.eclipse.che.ide.rest.AsyncRequestCallback;
 import org.eclipse.che.ide.rest.StringUnmarshaller;
@@ -37,11 +38,15 @@ import org.eclipse.che.ide.ui.dialogs.ConfirmCallback;
 import org.eclipse.che.ide.ui.dialogs.DialogFactory;
 
 import javax.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static org.eclipse.che.api.git.shared.DiffRequest.DiffType.NAME_STATUS;
+import static org.eclipse.che.api.git.shared.DiffRequest.DiffType.NAME_STATUS;;
 import static org.eclipse.che.ide.api.notification.StatusNotification.Status.FAIL;
 
 /**
@@ -60,7 +65,11 @@ public class RevertAction extends GitAction {
     private final GitLocalizationConstant locale;
     private final DtoFactory              dtoFactory;
 
-    private final static String REVISION = "HEAD";
+    private ProjectConfigDto              project;
+
+    private String                        projectPath;
+
+    private final static String           REVISION = "HEAD";
 
     @Inject
     public RevertAction(ComparePresenter presenter,
@@ -87,38 +96,75 @@ public class RevertAction extends GitAction {
     /** {@inheritDoc} */
     @Override
     public void actionPerformed(ActionEvent e) {
-        ProjectConfigDto project = appContext.getCurrentProject().getRootProject();
-        String pattern;
-        final String path;
+        project = appContext.getCurrentProject().getRootProject();
 
-        Selection<ResourceBasedNode<?>> selection = getExplorerSelection();
+        Selection<ResourceBasedNode< ? >> selection = getExplorerSelection();
 
         if (selection == null || selection.getHeadElement() == null) {
-            path = project.getPath();
+            return;
         } else {
-            path = ((HasStorablePath)selection.getHeadElement()).getStorablePath();
+            if (selection.isMultiSelection()) {
+                List<String> paths = new ArrayList<>();
+                for (ResourceBasedNode< ? > node : selection.getAllElements()) {
+                    paths.add(normalizePath(((HasStorablePath)node).getStorablePath()));
+                }
+                checkout(paths);
+            } else {
+                if (selection.getHeadElement() instanceof FolderReferenceNode) {
+                    gitService.diff(appContext.getWorkspaceId(), project,
+                                    Collections.singletonList(normalizePath(((HasStorablePath)selection.getHeadElement()).getStorablePath())),
+                                    NAME_STATUS, false, 0, REVISION, false, new AsyncRequestCallback<String>(new StringUnmarshaller()) {
+                                        @Override
+                                        protected void onSuccess(String result) {
+                                            if (result.isEmpty()) {
+                                                dialogFactory.createMessageDialog(locale.compareMessageIdenticalContentTitle(),
+                                                                                  locale.compareMessageIdenticalContentText(),
+                                                                                  new ConfirmCallback() {
+                                                                                      @Override
+                                                                                      public void accepted() {
+                                                                                          //Do nothing
+                                                                                      }
+                                                                                  }).show();
+                                            } else {
+                                                List<String> changedFiles = Arrays.asList(result.split("\n"));
+                                                checkout(changedFiles);
+                                            }
+                                        }
+
+                                        @Override
+                                        protected void onFailure(Throwable exception) {
+                                            notificationManager.notify(locale.diffFailed(), FAIL, false);
+                                        }
+                                    });
+                } else {
+                    checkout(Collections.singletonList(normalizePath(((HasStorablePath)selection.getHeadElement()).getStorablePath())));
+                }
+            }
         }
 
-        pattern = path.replaceFirst(project.getPath(), "");
-        pattern = (pattern.startsWith("/")) ? pattern.replaceFirst("/", "") : pattern;
 
-        CheckoutRequest checkoutRequest = dtoFactory.createDto(CheckoutRequest.class)
-                                                    .withFiles(Collections.singletonList(pattern))
-                                                    .withRevision(REVISION);
+    }
+
+    private void checkout(final List<String> paths) {
+        CheckoutRequest checkoutRequest = dtoFactory.createDto(CheckoutRequest.class).withFiles(paths).withRevision(REVISION);
         gitService.checkout(appContext.getWorkspaceId(), project, checkoutRequest, new AsyncRequestCallback<String>() {
             @Override
             protected void onSuccess(String result) {
-                eventBus.fireEvent(new FileContentUpdateEvent(path));
+                String projectPath = project.getPath();
+                for (String path : paths) {
+                    eventBus.fireEvent(new FileContentUpdateEvent(projectPath + "/" + path));
+                }
             }
 
             @Override
             protected void onFailure(Throwable exception) {
+                String s = exception.getMessage();
             }
         });
     }
 
-    private Selection<ResourceBasedNode<?>> getExplorerSelection() {
-        final Selection<ResourceBasedNode<?>> selection = (Selection<ResourceBasedNode<?>>)projectExplorer.getSelection();
+    private Selection<ResourceBasedNode< ? >> getExplorerSelection() {
+        final Selection<ResourceBasedNode< ? >> selection = (Selection<ResourceBasedNode< ? >>)projectExplorer.getSelection();
         if (selection == null || selection.isEmpty() || selection.getHeadElement() instanceof HasStorablePath) {
             return selection;
         } else {
@@ -126,14 +172,16 @@ public class RevertAction extends GitAction {
         }
     }
 
-    @Override
-    public void updateInPerspective(@NotNull ActionEvent event) {
-        event.getPresentation().setVisible(getActiveProject() != null);
-        event.getPresentation().setEnabled(isGitRepository() && compareSupported());
-    }
+    private String normalizePath(final String path) {
+        final String projectPath = project.getPath();
 
-    private boolean compareSupported() {
-        Selection selection = projectExplorer.getSelection();
-        return selection.isSingleSelection() && selection.getHeadElement() instanceof FileReferenceNode;
+        String pattern = path;
+        if (path.startsWith(projectPath)) {
+            pattern = pattern.replaceFirst(projectPath, "");
+        }
+        if (pattern.startsWith("/")) {
+            pattern = pattern.replaceFirst("/", "");
+        }
+        return pattern;
     }
 }
